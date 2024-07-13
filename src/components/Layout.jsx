e4237a47fc3cd7be1a8c052faada4358a0683d51
Artenlaclase <artenlaclase@gmail.com>
2024-07-13 00:20:43 -0400

    Navbar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import NavBar from './NavBar'
 
 export const Layout = () => {
   return (
     <div>
-      
+      <NavBar />
       <Outlet />
 
 
