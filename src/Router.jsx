59189f0a5647f02c7df084f39280dc6c422214a5
Artenlaclase <artenlaclase@gmail.com>
2024-07-13 00:08:26 -0400

    Rutas

---
+++ src/Router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
+
+export const Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/about',
+                element: <About/>
+            },
+        ]
+    }
+])
---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export const Layout = () => {
+  return (
+    <div>
+      
+      <Outlet />
+
+
+    </div>
+  )
+}
+
