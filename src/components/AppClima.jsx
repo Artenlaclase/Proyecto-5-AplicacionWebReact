2735c04c3b6b5611eb710ee8f02fbde6ba70c37d
Artenlaclase <artenlaclase@gmail.com>
2024-07-13 20:28:37 -0400

    error link appclima

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -13,7 +13,7 @@ export default function AppClima() {
     const [retry, setRetry] = useState(false);
 
     const [apiError, setApiError] = useState(null); // Nuevo estado para el error de la API
-    const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${query}`;
+    const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&q=${query}`;
 
 
     const { data, loading, error } = useFetch(query ? API_WEATHER : null);
